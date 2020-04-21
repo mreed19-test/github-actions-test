7f9ca25f05b1eaadb498ef1b348f44d724bb7984
Michael Reed <michaelr@sb401k.com>
2020-04-21 10:15:45 -0700

    I guess workflows was just being creazy slow

--- trigger_workflow.sh
+++ trigger_workflow.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
 
-# echo "Select Workflow to trigger:"
-# echo "1) manual_trigger"
-# echo "2) docker_build"
+echo "Select Workflow to trigger:"
+echo "1) manual_trigger"
+echo "2) docker_build"
 
-# read selection
+read selection
 
-# if [[ $selection == '1' ]]; then
-#   event_type='manual_trigger'
-# elif [[ $selection == '2' ]]; then
-#   event_type='docker_build'
-# else
-#   echo "Invalid Input: $selection"
-#   echo "Input must be 1 or 2"
-#   exit 1
-# fi
+if [[ $selection == '1' ]]; then
+  event_type='manual_trigger'
+elif [[ $selection == '2' ]]; then
+  event_type='docker_build'
+else
+  echo "Invalid Input: $selection"
+  echo "Input must be 1 or 2"
+  exit 1
+fi
 
-# echo "{\"event_type\": \"$event_type\"}"
-
-# curl -H "Accept: application/vnd.github.everest-preview+json" \
-#   -H "Authorization: token $GITHUB_ACTIONS_TEST_TOKEN" \
-#   --request POST \
-#   --data "{\"event_type\": \"$event_type\"}" \
-#   -w "%{http_code}\n" \
-#   https://api.github.com/repos/mreed19/github-actions-test/dispatches
+echo "{\"event_type\": \"$event_type\"}"
 
 curl -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Authorization: token $GITHUB_ACTIONS_TEST_TOKEN" \
   --request POST \
-  --data '{"event_type": "docker_build"}' \
+  --data "{\"event_type\": \"$event_type\"}" \
   -w "%{http_code}\n" \
   https://api.github.com/repos/mreed19/github-actions-test/dispatches
