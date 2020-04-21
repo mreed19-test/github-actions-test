468974d94453322288adfd2dbf779582646794b5
Michael Reed <michaelr@sb401k.com>
2020-04-21 09:31:47 -0700

    Try to add workflow to build and push a docker image to github packages. If this doesn't work we will use ECR

--- trigger_workflow.sh
+++ trigger_workflow.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+echo "Select Workflow to trigger:"
+echo "1) manual_trigger"
+echo "2) docker_build"
+
+read selection
+
+if [[ $selection == '1' ]]; then
+  event_type='manual_trigger'
+elif [[ $selection == '2' ]]; then
+  event_type='docker_build'
+else
+  echo "Invalid Input: $selection"
+  echo "Input must be 1 or 2"
+  exit 1
+fi
+
 curl -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Authorization: token $GITHUB_ACTIONS_TEST_TOKEN" \
   --request POST \
-  --data '{"event_type": "manual_trigger"}' \
+  --data "{\"event_type\": \"$event_type\"}" \
   https://api.github.com/repos/mreed19/github-actions-test/dispatches
